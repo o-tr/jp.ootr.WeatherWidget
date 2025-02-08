26badb23d1882015a7ed8e71287a5697d2d2b385
ootr <git@ootr.jp>
2025-02-09 05:39:01 +0900

    設定画面の文言を多言語化

--- Runtime/jp.ootr.WeatherWidget/Scripts/24_Settings.cs
+++ Runtime/jp.ootr.WeatherWidget/Scripts/24_Settings.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace jp.ootr.WeatherWidget
 {
     public class Settings : Copy
     {
+        [SerializeField] private TextMeshProUGUI _settingsTitle;
         private readonly int _animatorShow = Animator.StringToHash("Settings");
         private bool _isShown;
 
@@ -11,6 +13,12 @@ namespace jp.ootr.WeatherWidget
         {
             _isShown = !_isShown;
             animator.SetBool(_animatorShow, _isShown);
+        }
+
+        protected override void OnWeatherLoadSuccess(WeatherData data)
+        {
+            base.OnWeatherLoadSuccess(data);
+            _settingsTitle.text = GetText("settings", data.GetLocale());
         }
     }
 }
